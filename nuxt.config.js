59b428798f08c73017399afeb767716c891cfab2
Benedicte Raae <raae@bgraphic.no>
2017-08-04 11:53:53 +0200

    Better meta in head

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'NRP',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Lytt til norskspråklige podkaster' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
