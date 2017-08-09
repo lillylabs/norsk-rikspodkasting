abf6183ad72010f5bb4f273bc547a0ae41f4264e
Benedicte Raae <raae@bgraphic.no>
2017-08-09 23:12:01 +0200

    Norwegian date months.

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import moment from 'moment'
+moment.locale('nb', {
+  monthsShort: 'jan_feb_mars_april_mai_juni_juli_aug_sep_okt_nov_des'.split('_')
+})
 
 Vue.filter('capitalize', val => val.toUpperCase())
 
